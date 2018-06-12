21d186a06cee3f755155f41e960d1434a8ef6b1f
dustine32 <debert@usc.edu>
2018-06-12 08:22:45 -0700

    api call

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from db_caller import get_query
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=('GET', 'POST'))
 def what_up():
@@ -14,3 +16,11 @@ def what_up():
         results = get_query("gene2enhancers", {"gene": term})
         # return str(request.form)
     return render_template("search.html", results=results, result_count=len(results))
+
+@app.route("/search/<gene>")
+def search(gene):
+    results = get_query("gene2enhancers", {"gene": gene})
+    return jsonify(results)
+
+#### Startup on remote server:
+#### FLASK_APP=server.py flask run -h 0.0.0.0

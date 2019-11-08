61e758ef549bd174d9144f8e6a33b5dda3d00ccd
dustine32 <dustine32@gmail.com>
2019-11-08 11:19:45 -0800

    Refactored data2json converter for Caitlin's new data

--- data2json_converter/converter.py
+++ data2json_converter/converter.py
@@ -1,9 +1,13 @@
+import csv
 import json
 import argparse
 from os import path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', "--raw-file", type=str, required=False)
+parser.add_argument('-e', "--enhancer_file")
+parser.add_argument('-a', "--assay_file")
+parser.add_argument('-t', "--tissue_file")
 parser.add_argument('-s', "--suffix", type=str, required=False)
 parser.add_argument('-c', "--col-map", type=str, required=False)
 parser.add_argument('-d', "--delimiter", type=str, required=False)
@@ -16,52 +20,78 @@ column_mapping = [
     'tissue',
     'assay'
 ]
-ASSAY_LOOKUP = {
-"1": "ChIA-PET",
-"2": "single-tissue eQTL",
-"3": "Topologically Associated Domain",
-"4": "DNAse Hyersensitivity Region, histone acetylation marks"
-}
-TISSUE_LOOKUP = {}
-with open("resources/tissuetable") as tf:
-    for tline in tf.readlines():
-        tnum, tname = tline.split("\t")
-        TISSUE_LOOKUP[str(tnum)] = tname.rstrip()
 
-def writeout(jsons, suffix):
-    with open("{}.json".format(suffix), "a+") as j:
+def parse_lookup_file(filepath):
+    lookup = {}
+    with open(filepath) as f:
+        reader = csv.reader(f, delimiter="\t")
+        for r in reader:
+            # Ex: ['1', 'Adipose Subcutaneous']
+            lookup[r[0]] = r[1]
+    return lookup
+
+
+def parse_enhancer_file(filepath):
+    lookup = {}
+    with open(filepath) as f:
+        reader = csv.reader(f, delimiter="\t")
+        for r in reader:
+            # Ex: ['chr1', '99534632', '99534837', '1']
+            lookup[r[3]] = {
+                "chrNum": r[0],
+                "start": r[1],
+                "end": r[2],
+            }
+    return lookup
+
+
+ASSAY_LOOKUP = None
+TISSUE_LOOKUP = None
+COORDINATES_LOOKUP = None  # TODO: Enhancer coordinates files
+
+def writeout(jsons):
+    # with open("out.json", "a+") as j:
+    with open("out.json", "w+") as j:
         json.dump(jsons, j, indent=4)
 
-def generate_json(raw_file, suffix, col_map=column_mapping, parse_col=False, delimiter=None):
+def generate_json(raw_file, col_map=column_mapping, parse_col=False, delimiter=None):
+    # rows[0]
+    # ['enhancer', 'gene', 'linkID', 'assay', 'tissue', 'p-value', 'eQTL_SNP_ID']
+    # rows[3]
+    # ['1', 'HUMAN|HGNC=15846|UniProtKB=Q9NP74', '1', '3', '66', '', '']
+    # rows[20]
+    # ['1', 'HUMAN|HGNC=15846|UniProtKB=Q9NP74', '1', '4', '53', '6e-08', '']
+
     bunchsize = 1000000
     new_jsons = []
     if delimiter is None:
         delimiter = "\t"
     with open(raw_file) as f:
+        reader = csv.reader(f, delimiter="\t")
         if parse_col:
-            # print("hey")
-            col_map = f.readline().rstrip().split(delimiter)
+            col_map = next(reader)
         elif col_map is None:
             col_map = column_mapping
         elif col_map.__class__ is str:
             col_map = col_map.split(",")
-        # print(col_map)
-        for l in f.readlines():
+        for r in reader:
             new_line = {}
-            line_values = l.split(delimiter)
-            for idx, val in enumerate(line_values):
-                val = val.rstrip()
-                if col_map[idx] == "assay":
-                    val = ASSAY_LOOKUP[str(val)]
-                if col_map[idx] == "tissue":
-                    val = TISSUE_LOOKUP[str(val)]
-                new_line["{}_{}".format(suffix, col_map[idx])] = val
+            for idx, val in enumerate(r):
+                if len(val) > 0:
+                    if col_map[idx] == "enhancer":
+                        # fancy way to merge coord info to new_line
+                        new_line = {**new_line, **COORDINATES_LOOKUP[val]}
+                    if col_map[idx] == "assay":
+                        val = ASSAY_LOOKUP[str(val)]
+                    if col_map[idx] == "tissue":
+                        val = TISSUE_LOOKUP[str(val)]
+                    new_line[col_map[idx]] = val
             new_jsons.append(new_line)
             if len(new_jsons) == bunchsize:
-                print("writing " + suffix)
-                writeout(new_jsons, suffix)
+                print("writing files out")
+                writeout(new_jsons)
                 new_jsons = []
-    writeout(new_jsons, suffix)
+    writeout(new_jsons)
 
 def check_file(file_path):
     if path.isfile(file_path):
@@ -70,21 +100,30 @@ def check_file(file_path):
         print("File path '" + file_path + "' does not exist.")
         return False
 
-def parse_file(raw_file=None, suffix=None, col_map=None, parse_col=False, delimiter=None):
+def parse_file(raw_file=None, col_map=None, parse_col=False, delimiter=None):
     # print(col_map.__class__.__name__)
     if raw_file is None:
         for suffix in ["chia","eqtl","tad"]:
             raw_file = "raw/linksDB{}".format(suffix)
             if check_file(raw_file):
-                generate_json(raw_file, suffix, col_map, parse_col, delimiter)
+                generate_json(raw_file, col_map, parse_col, delimiter)
 
     else:
-        generate_json(raw_file, suffix, col_map, parse_col, delimiter)
+        generate_json(raw_file, col_map, parse_col, delimiter)
 
 
 def main():
     args = parser.parse_args()
-    parse_file(args.raw_file, args.suffix, args.col_map, args.parse_col, args.delimiter)
+
+    global ASSAY_LOOKUP
+    ASSAY_LOOKUP = parse_lookup_file(args.assay_file)
+    global TISSUE_LOOKUP
+    TISSUE_LOOKUP = parse_lookup_file(args.tissue_file)
+    global COORDINATES_LOOKUP
+    COORDINATES_LOOKUP = parse_enhancer_file(args.enhancer_file)
+
+    parse_file(args.raw_file, args.col_map, args.parse_col, args.delimiter)
+    # TODO: Add docs for unlinked enhancers
 
 if __name__ == "__main__":
     main()
